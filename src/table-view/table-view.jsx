b4db774c8594f5699a94e524f086556f87aff860
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-08-01 23:26:42 +0200

    Add useful comments

--- src/table-view/table-view.jsx
+++ src/table-view/table-view.jsx
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button'
 
 import Select from '../select'
 import Table from '../table'
-import { withStyles } from '@material-ui/core'
 
 const styles = () => ({
   filtersBox: {
@@ -21,8 +20,13 @@ const styles = () => ({
 const TableView = createReactClass({
   displayName: 'TableView',
   propTypes: {
+    //The data array to view in the table
     rows: PropTypes.array,
+
+    //The filters options (a selectbox is created for each one)
     filterBy: PropTypes.array,
+
+    //The table headers
     columns: PropTypes.array
   },
   getDefaultProps () {
@@ -32,25 +36,34 @@ const TableView = createReactClass({
       columns: []
     }
   },
+
   getInitialState () {
+    //Will hold the state of each select box (how many filter value is selected for each)
     return { filterSelects: {} }
   },
+
   setSelectedFilter (event, filterByElement) {
     this.setState(state => {
       return { ...state, filterSelects: { ...state.filterSelects, [filterByElement]: event.target.value } }
     })
   },
+
   clearFilters () {
     this.setState(state => {
       return { ...state, filterSelects: {} }
     })
   },
+
   render () {
     const { rows, filterBy, columns } = this.props
 
+    //Display the "Clear Filters" button if only there is any selected values
     const showButton = Object.keys(this.state.filterSelects).some((filterSelectKey) => this.state.filterSelects[filterSelectKey].length > 0)
 
+    //transform each filter option to a select box
     const selects = filterBy.map((filterByElement, index) => {
+      
+      //get all the possible values that the user can select from
       const allPossibleValuesForField = rows.reduce((acc, item) => {
         acc[item[filterByElement.dataKey]] = undefined
         return acc
@@ -66,9 +79,12 @@ const TableView = createReactClass({
       />)
     })
 
+    //Filter the data
     const filteredData = rows.filter(row => {
       return Object.keys(this.state.filterSelects).every((filterSelectKey) => {
         const filterSelectedValues = this.state.filterSelects[filterSelectKey]
+        
+        //if there is a select options, use it, otherwise pass all the values
         return filterSelectedValues.length > 0 ? filterSelectedValues.some(filterSelectedValue => `${row[filterSelectKey]}` === `${filterSelectedValue}`) : true
       })
     })
